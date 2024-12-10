7b23c731ee965415103ed686202d791ffd05366d
LoiNguyennn <nguyenphucloi231@gmail.com>
2024-12-10 12:29:49 +0700

    #19-LoiNguyennn-fix edit article

--- src/routers/viewRouter.ts
+++ src/routers/viewRouter.ts
@@ -11,7 +11,7 @@ viewRouter.get("/signup", viewController.getSignupPage);
 viewRouter.get("/create_user", viewController.getCreateUserPage);
 
 viewRouter.get("/update_profile", authenticateJWT, viewController.getUpdateUserProfilePage);
-viewRouter.get("/create_article", viewController.getCreateArticlePage);
+viewRouter.get("/create_article", authenticateJWT, viewController.getCreateArticlePage);
 viewRouter.get("/article/:id", authenticateJWT, viewController.getArticlePage);
 
 viewRouter.get("/edit_article/:articleId", authenticateJWT, viewController.getEditArticlePage);

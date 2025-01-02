1b84e5cf97e41d338d2822801c5cc71562d806cc
Luong <tnhluong22@clc.fitus.edu.vn>
2025-01-03 02:42:52 +0700

    #96-gnoulh-fix-header-and-add-articles

--- src/routers/categoriesRouter.ts
+++ src/routers/categoriesRouter.ts
@@ -9,6 +9,7 @@ categoryRouter.get("/", categoryController.getAllCategories);
 categoryRouter.post("/create", categoryController.createCategory);
 categoryRouter.put("/update/:id", categoryController.updateCategory);
 categoryRouter.delete("/delete/:id", categoryController.deleteCategory);
+categoryRouter.get("/header", categoryController.getCategories);
 
 export default categoryRouter;
 

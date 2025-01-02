408bb7c798f3e7def25a3ab2d4aab4199bf9e94b
Nguyễn Bá An <127473341+nban22@users.noreply.github.com>
2025-01-02 17:27:04 +0700

    #28-nban22-[Front-end] Update Layout for Authentication Section (#98)

--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -24,20 +24,22 @@ export const getHomePage = catchAsync(async (req: Request, res: Response, next: 
     const user = req.body.user;
 
     const latestArticles = (await articleController.getLatestArticles()).data.articles;
-    
 
-    const featuredArticles = (await Article.getFeaturedArticles()).map(article => ({
+    const featuredArticles = (await Article.getFeaturedArticles()).map((article) => ({
         ...article.toObject(),
         summary: article.summary?.replace(/<\/?[^>]+(>|$)/g, ""),
-        publish_date: moment(article.publish_date).format('DD-MM-YYYY'),
+        publish_date: moment(article.publish_date).format("DD-MM-YYYY"),
     }));
 
-    const topCategories = (await fetchTopCategories()).map(category => ({
+    const topCategories = (await fetchTopCategories()).map((category) => ({
         ...category,
-        publishDate: moment(category.publishDate).format('DD-MM-YYYY'),
-        }));
-    
+        publishDate: moment(category.publishDate).format("DD-MM-YYYY"),
+    }));
+
+    console.log(latestArticles);
+
     res.status(StatusCodes.OK).render("pages/home", {
+        layout: false,
         user: user,
         latestArticle: latestArticles,
         featuredArticles: featuredArticles,
@@ -46,48 +48,66 @@ export const getHomePage = catchAsync(async (req: Request, res: Response, next: 
 });
 
 export const getLoginPage = (req: Request, res: Response, next: NextFunction) => {
-    res.status(StatusCodes.OK).render("pages/login");
+    res.status(StatusCodes.OK).render("pages/auth/login", {
+        layout: "layouts/auth",
+        title: "Đăng nhập",
+        scripts: `<script src="/js/pages/login.js"></script>`,
+    });
 };
 
 export const getForgotPasswordPage = (req: Request, res: Response, next: NextFunction) => {
     const { enterCode, email } = req.query;
-    res.status(StatusCodes.OK).render("pages/forgot_password", { enterCode, email });
+    res.status(StatusCodes.OK).render("pages/auth/forgot_password", {
+        layout: "layouts/auth",
+        title: "Quên mật khẩu",
+        scripts: `<script src="/js/pages/forgot_password.js"></script>`,
+        enterCode,
+        email,
+    });
 };
 
 export const getResetPasswordPage = (req: Request, res: Response, next: NextFunction) => {
-    res.status(StatusCodes.OK).render("pages/reset_password");
-}
+    res.status(StatusCodes.OK).render("pages/auth/reset_password", {
+        layout: "layouts/auth",
+        title: "Đặt lại mật khẩu",
+        scripts: `<script src="/js/pages/reset_password.js"></script>`,
+    });
+};
 
 export const getSignupPage = (req: Request, res: Response, next: NextFunction) => {
-    res.status(StatusCodes.OK).render("pages/signup");
-}
+    res.status(StatusCodes.OK).render("pages/auth/signup", {
+        layout: "layouts/auth",
+        title: "Đăng ký",
+        scripts: `<script src="/js/pages/signup.js"></script>`,
+    });
+};
 
-export const getCreateUserPage = (req: Request, res: Response, next: NextFunction) => { 
+export const getCreateUserPage = (req: Request, res: Response, next: NextFunction) => {
     res.status(StatusCodes.OK).render("create_user");
-}
+};
 
 export const getUpdateUserProfilePage = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.body.userId;
     const user = req.body.user;
-    
+
     let profileOwner: any;
     if (user.role === "subscriber") {
-        profileOwner = await SubscriberProfile.findOne({user_id: userId});
+        profileOwner = await SubscriberProfile.findOne({ user_id: userId });
     } else if (user.role === "writer") {
-        profileOwner = await WriterProfile.findOne({user_id: userId});
+        profileOwner = await WriterProfile.findOne({ user_id: userId });
     } else if (user.role === "editor") {
-        profileOwner = await EditorProfile.findOne({user_id: userId});
+        profileOwner = await EditorProfile.findOne({ user_id: userId });
     } else {
         return next(new AppError(StatusCodes.NOT_FOUND, "No profile found for this user!"));
     }
-    
-    res.status(StatusCodes.OK).render("pages/update_profile", {user: user, profile: profileOwner});
+
+    res.status(StatusCodes.OK).render("pages/update_profile", { user: user, profile: profileOwner });
 });
 
 export const getCreateArticlePage = (req: Request, res: Response, next: NextFunction) => {
     const user = req.body.user;
-    res.status(StatusCodes.OK).render("pages/create_article", {user: user, article: null});
-}   
+    res.status(StatusCodes.OK).render("pages/create_article", { user: user, article: null });
+};
 
 export const getArticlePage = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const articleId = req.params.id;
@@ -108,16 +128,16 @@ export const getArticlePage = catchAsync(async (req: Request, res: Response, nex
     if (!article) {
         return next(new AppError(StatusCodes.NOT_FOUND, "Article not found!"));
     }
-    
+
     if (article.is_premium && req.body.user === null) {
         const message = "Bài viết này chỉ dành cho người đăng ký thành viên!";
         return res.status(StatusCodes.FORBIDDEN).render("pages/access_denied", { message });
     }
 
-    if (article.is_premium && req.body.user.role === 'subscriber') {
+    if (article.is_premium && req.body.user.role === "subscriber") {
         const message = "Tài khoản của bạn đã hết hạn!";
-        const subscriber = await SubscriberProfile.findOne({ user_id: req.body.user._id }); 
-        if (subscriber && subscriber.subscription_status === 'expired') {
+        const subscriber = await SubscriberProfile.findOne({ user_id: req.body.user._id });
+        if (subscriber && subscriber.subscription_status === "expired") {
             return res.status(StatusCodes.FORBIDDEN).render("pages/access_denied", { message });
         }
     }
@@ -145,19 +165,15 @@ export const getArticlePage = catchAsync(async (req: Request, res: Response, nex
     // Lấy các thẻ của bài viết
     const tagsListID = await ArticleTag.find({ article_id: articleObjectId }).select("tag_id");
 
-    const tags = await Tag.find({ _id: { $in: tagsListID.map(tag => tag.tag_id) } });
+    const tags = await Tag.find({ _id: { $in: tagsListID.map((tag) => tag.tag_id) } });
 
-    const tagNames = tags.map(tag => tag.name);
+    const tagNames = tags.map((tag) => tag.name);
 
     // Lấy tất cả bình luận cho bài viết
-    const comments = await Comment.find({ article_id: articleObjectId })
-        .populate("user_id create_at content")
-        .exec();
-
+    const comments = await Comment.find({ article_id: articleObjectId }).populate("user_id create_at content").exec();
 
     // Lấy tất cả người dùng từ User
-    const users = await User.find().select("_id name"); 
-
+    const users = await User.find().select("_id name");
 
     const userMap = new Map(users.map((user) => [user._id.toString(), user.name]));
 
@@ -165,9 +181,10 @@ export const getArticlePage = catchAsync(async (req: Request, res: Response, nex
         return {
             content: comment.content,
             date: comment.create_at,
-            userName: comment.user_id && userMap.has(comment.user_id._id.toString())
-                ? userMap.get(comment.user_id._id.toString()) // Lấy tên người dùng từ Map
-                : "Ẩn danh",
+            userName:
+                comment.user_id && userMap.has(comment.user_id._id.toString())
+                    ? userMap.get(comment.user_id._id.toString()) // Lấy tên người dùng từ Map
+                    : "Ẩn danh",
         };
     });
 
@@ -180,7 +197,7 @@ export const getArticlePage = catchAsync(async (req: Request, res: Response, nex
         .sort({ created_at: -1 })
         .select("title publish_date thumbnail")
         .exec();
-        
+
     const formattedArticles = relatedArticles.map((article) => ({
         ...article.toObject(),
         publish_date: moment(article.publish_date).format("DD-MM-YYYY"), // Định dạng ngày
@@ -204,7 +221,7 @@ export const getArticlePage = catchAsync(async (req: Request, res: Response, nex
             categoryName,
             writerName,
             tagNames,
-            comments: formattedComments, 
+            comments: formattedComments,
             relatedArticles: formattedArticles,
             publish_date: formattedPublishDate,
         },
@@ -217,30 +234,32 @@ export const getEditArticleForm = catchAsync(async (req: Request, res: Response,
 
     const user = req.body.user;
 
-    const writer = await WriterProfile.findOne({ user_id: user._id })
+    const writer = await WriterProfile.findOne({ user_id: user._id });
     if (!writer) {
         return next(new AppError(StatusCodes.NOT_FOUND, "No profile found for this user!"));
     }
 
     // status draft or rejected
-    const article = await Article.findOne({_id: articleId, status: { $in: ["draft", "rejected"] }}).
-                                    populate("category_id")
+    const article = await Article.findOne({ _id: articleId, status: { $in: ["draft", "rejected"] } }).populate(
+        "category_id"
+    );
     if (!article) {
         return next(new AppError(StatusCodes.NOT_FOUND, "Article not found!"));
     }
 
-    if (article.writer_id.toString() !== writer._id?.toString()) {   
+    if (article.writer_id.toString() !== writer._id?.toString()) {
         return next(new AppError(StatusCodes.FORBIDDEN, "You are not authorized to edit this article!"));
     }
-    
-    res.status(StatusCodes.OK).render("pages/edit_article", {user: user, article: article});
+
+    res.status(StatusCodes.OK).render("pages/edit_article", { user: user, article: article });
 });
 
 export const getCategoryArticleList = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = req.body.user;
 
-    const category = req.params.categoryName.replace('-', ' ').charAt(0).toUpperCase()
-                    + req.params.categoryName.replace('-', ' ').slice(1);
+    const category =
+        req.params.categoryName.replace("-", " ").charAt(0).toUpperCase() +
+        req.params.categoryName.replace("-", " ").slice(1);
     if (!category) {
         throw next(new AppError(StatusCodes.BAD_REQUEST, "Please provide category name"));
     }
@@ -250,15 +269,15 @@ export const getCategoryArticleList = catchAsync(async (req: Request, res: Respo
     res.status(StatusCodes.OK).render("pages/category_articles", {
         user: user,
         category: article.data.category,
-        articles: article.data.articles
-    })
-})
+        articles: article.data.articles,
+    });
+});
 
 export const getTagArticleList = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const user = req.body.user;
 
-    const tag = req.params.tagName.replace('-', ' ').charAt(0).toUpperCase()
-                    + req.params.tagName.replace('-', ' ').slice(1);
+    const tag =
+        req.params.tagName.replace("-", " ").charAt(0).toUpperCase() + req.params.tagName.replace("-", " ").slice(1);
     if (!tag) {
         throw next(new AppError(StatusCodes.BAD_REQUEST, "Please provide tag name"));
     }
@@ -268,8 +287,8 @@ export const getTagArticleList = catchAsync(async (req: Request, res: Response, 
     res.status(StatusCodes.OK).render("pages/tag_articles", {
         user: user,
         tag: article.data.tag,
-        articles: article.data.articles
-    })
+        articles: article.data.articles,
+    });
 });
 
 export const getWriterArticleList = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -288,112 +307,101 @@ export const getWriterArticleList = catchAsync(async (req: Request, res: Respons
     const totalArticles = await Article.countDocuments({ writer_id: writer._id });
 
     // Fetch articles with pagination
-    const articles = (await Article.find({ writer_id: writer._id })
-        .skip(skip)
-        .limit(limit)).map(article => ({
-        ...article.toObject(),
-        summary: sanitizeSummary(article.summary || ''),
-        content: sanitizeContent(article.content),
-        created_at: moment(article.created_at).format('DD-MM-YYYY'),
-    }))
-    .sort((a, b) => {
-        const getStatusPriority = (article: any) => {
-            if (article.status === "published") return 3;
-            if (article.status === "rejected") return 2;
-            if (article.status === "draft") return 1;
-            return 0;
-        };
+    const articles = (await Article.find({ writer_id: writer._id }).skip(skip).limit(limit))
+        .map((article) => ({
+            ...article.toObject(),
+            summary: sanitizeSummary(article.summary || ""),
+            content: sanitizeContent(article.content),
+            created_at: moment(article.created_at).format("DD-MM-YYYY"),
+        }))
+        .sort((a, b) => {
+            const getStatusPriority = (article: any) => {
+                if (article.status === "published") return 3;
+                if (article.status === "rejected") return 2;
+                if (article.status === "draft") return 1;
+                return 0;
+            };
 
-        const statusPriorityA = getStatusPriority(a);
-        const statusPriorityB = getStatusPriority(b);
+            const statusPriorityA = getStatusPriority(a);
+            const statusPriorityB = getStatusPriority(b);
 
-        // First, compare based on status (published > rejected > draft)
-        if (statusPriorityA !== statusPriorityB) {
-            return statusPriorityB - statusPriorityA;
-        }
+            // First, compare based on status (published > rejected > draft)
+            if (statusPriorityA !== statusPriorityB) {
+                return statusPriorityB - statusPriorityA;
+            }
 
-        // If statuses are the same, compare based on premium
-        if (a.is_premium !== b.is_premium) {
-            return b.is_premium ? 1 : -1;  // Premium first (highest priority)
-        }
+            // If statuses are the same, compare based on premium
+            if (a.is_premium !== b.is_premium) {
+                return b.is_premium ? 1 : -1; // Premium first (highest priority)
+            }
 
-        return 0; // If both status and premium are the same, maintain original order
-    });
+            return 0; // If both status and premium are the same, maintain original order
+        });
 
     // Calculate total pages
     const totalPages = Math.ceil(totalArticles / limit);
 
     // Render the page with pagination data
-    res.status(StatusCodes.OK).render("pages/writer_articles", { 
-        user: user, 
+    res.status(StatusCodes.OK).render("pages/writer_articles", {
+        user: user,
         articles: articles,
         page: page,
-        totalPages: totalPages
+        totalPages: totalPages,
     });
 });
-
-
-
 
 export const getSearchPage = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const page = parseInt(req.query.page as string) || 1;
         const limit = parseInt(req.query.limit as string) || 8;
-        const search = req.query.q as string || '';
-        const sortField = req.query.sort as string || 'view_count';
-        const sortOrder = req.query.order as string || 'desc';
+        const search = (req.query.q as string) || "";
+        const sortField = (req.query.sort as string) || "view_count";
+        const sortOrder = (req.query.order as string) || "desc";
         const user = req.body.user;
-    
-    
+
         // Search query for title, summary, and content
         const searchQuery = {
             $or: [
-                { title: { $regex: search, $options: 'i' } },
-                { summary: { $regex: search, $options: 'i' } },
-                { content: { $regex: search, $options: 'i' } },
+                { title: { $regex: search, $options: "i" } },
+                { summary: { $regex: search, $options: "i" } },
+                { content: { $regex: search, $options: "i" } },
             ],
             // status: 'published',
         };
-    
+
         // Pagination calculation: (page - 1) * limit
         const startIndex = (page - 1) * limit;
-    
+
         // Sorting options
-        const sortOptions: { [key: string]: 1 | -1 } = { [sortField]: sortOrder === 'desc' ? -1 : 1 };
-    
+        const sortOptions: { [key: string]: 1 | -1 } = { [sortField]: sortOrder === "desc" ? -1 : 1 };
+
         // Fetching articles with pagination and sorting
-        const articles = await Article.find(searchQuery)
-            .skip(startIndex)
-            .limit(limit)
-            .sort(sortOptions); 
-    
-            const categories = await Category.find({ _id: { $in: articles.map((article) => article.category_id) } });
-            const tags = await Tag.find({ _id: { $in: articles.map((article) => article._id) } });
-    
+        const articles = await Article.find(searchQuery).skip(startIndex).limit(limit).sort(sortOptions);
+
+        const categories = await Category.find({ _id: { $in: articles.map((article) => article.category_id) } });
+        const tags = await Tag.find({ _id: { $in: articles.map((article) => article._id) } });
 
         // format publish date
         const formattedArticles = articles.map((article) => ({
             ...article.toObject(),
-            publish_date: moment(article.publish_date).format('DD-MM-YYYY'),
+            publish_date: moment(article.publish_date).format("DD-MM-YYYY"),
         }));
 
-
-
-        
         // Mapping articles to display relevant data
         const results = formattedArticles.map((article) => ({
             _id: article._id,
             title: article.title,
-            category: categories.find((category) => category._id === article.category_id)?.name || 'Tự do',
+            category: categories.find((category) => category._id === article.category_id)?.name || "Tự do",
             tags: tags.find((tag) => tag._id === article._id)?.name || [],
             thumbnail: article.thumbnail || null,
-            summary: sanitizeSummary(article.summary || ''), 
-            publishDate: article.publish_date || 'Gần đây',
+            summary: sanitizeSummary(article.summary || ""),
+            publishDate: article.publish_date || "Gần đây",
             viewCount: article.view_count || 0,
-            sortField: sortField,}))
-    
+            sortField: sortField,
+        }));
+
         // Return the results, or a message if no articles are found
-        res.render('pages/search', {
+        res.render("pages/search", {
             user,
             articles: results,
             page,
@@ -401,8 +409,10 @@ export const getSearchPage = async (req: Request, res: Response, next: NextFunct
             search,
             sortField,
             sortOrder,
-            message: results.length ? `Kết quả tìm kiếm cho: ${search}` : 'Không có bài viết nào phù hợp với từ khóa tìm kiếm của bạn.',
-            suggestion: results.length ? null : 'Hãy thử lại với từ khóa khác!',
+            message: results.length
+                ? `Kết quả tìm kiếm cho: ${search}`
+                : "Không có bài viết nào phù hợp với từ khóa tìm kiếm của bạn.",
+            suggestion: results.length ? null : "Hãy thử lại với từ khóa khác!",
         });
     } catch (error) {
         next(error);

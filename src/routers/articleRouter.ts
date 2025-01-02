abf58fc8818e24938aa0801680b9777015cec753
LoiNguyennn <127386199+LoiNguyennn@users.noreply.github.com>
2025-01-02 21:09:13 +0700

    LoiNguyennn-Fix View list of articles by category and tag + dynamically load category to header (#99)

--- src/routers/articleRouter.ts
+++ src/routers/articleRouter.ts
@@ -13,7 +13,7 @@ const articleRouter = Router();
 articleRouter.use(attachUserId); // Middleware to attach user_id to the request object
 
 articleRouter.post("/", authorizeRole(["writer"]), upload.single("image"), articleController.createArticle); // Route to create a new article
-articleRouter.post("/upload", authorizeRole(["writer"]), upload.single("image"), articleController.uploadImage); // Route to upload an image
 articleRouter.put("/:articleId", authorizeRole(["writer"]), articleController.updateArticle); // Route to update an article
+articleRouter.post("/upload", authorizeRole(["writer"]), upload.single("image"), articleController.uploadImage); // Route to upload an image
 
 export default articleRouter;
